8828712607e0e4cee07ea35f5966b33a5bca0379
Uridrack7 <Uridrack7@DESKTOP-EUGRMV1>
2017-11-14 00:07:35 -0600

    Excepciones clase 13/11/17

---
+++ src/excepciones1/ExcepcionesUno.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones1;
+
+/**
+ *
+ * @author Uridrack7
+ */
+public class ExcepcionesUno {
+    public static void main(String[] args) {
+        
+        //Esta excepcion es muy conocida
+        int x=0;
+        float y=2;
+        
+        System.out.println("Antes");
+        System.out.println(y/x);
+        System.out.println("Despues");
+    }
+}
